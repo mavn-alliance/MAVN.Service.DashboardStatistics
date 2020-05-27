04a5123de1c45881d32e40da6824bcb837663a5d
Sergey Nesterov <sergey.nesterov@lykke.com>
2020-05-27 18:23:13 +0300

    Nuget updates + rabbit registrations fix

---
+++ src/MAVN.Service.DashboardStatistics/Modules/RabbitMqModule.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using JetBrains.Annotations;
+using Lykke.Common;
+using Lykke.SettingsReader;
+using MAVN.Service.DashboardStatistics.Rabbit.Subscribers;
+using MAVN.Service.DashboardStatistics.Settings;
+using MAVN.Service.DashboardStatistics.Settings.Service.Rabbit;
+
+namespace MAVN.Service.DashboardStatistics.Modules
+{
+    [UsedImplicitly]
+    public class RabbitMqModule : Module
+    {
+        private const string QueueName = "dashboard-statistic";
+        private const string BonusReceivedExchangeName = "lykke.wallet.bonusreceived";
+        private const string LeadStateChangedExchangeName = "lykke.bonus.leadstatechanged";
+        private const string PartnersPaymentTokensReservedExchangeName = "lykke.wallet.partnerspaymenttokensreserved";
+        private const string CustomerPhoneVerifiedEventExchangeName = "lykke.customer.phoneverified";
+
+        private readonly RabbitMqSettings _settings;
+
+        public RabbitMqModule(IReloadingManager<AppSettings> settings)
+        {
+            _settings = settings.CurrentValue.DashboardStatisticsService.RabbitMq;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Subscribers
+
+            builder.RegisterType<BonusReceivedEventSubscriber>()
+                .As<IStartStop>()
+                .WithParameter("connectionString", _settings.WalletManagementRabbitMqConnectionString)
+                .WithParameter("exchangeName", BonusReceivedExchangeName)
+                .WithParameter("queueName", QueueName);
+
+            builder.RegisterType<LeadStateChangedSubscriber>()
+                .As<IStartStop>()
+                .WithParameter("connectionString", _settings.ReferralRabbitMqConnectionString)
+                .WithParameter("exchangeName", LeadStateChangedExchangeName)
+                .WithParameter("queueName", QueueName);
+
+            builder.RegisterType<PartnersPaymentTokensReservedEventSubscriber>()
+                .As<IStartStop>()
+                .WithParameter("connectionString", _settings.WalletManagementRabbitMqConnectionString)
+                .WithParameter("exchangeName", PartnersPaymentTokensReservedExchangeName)
+                .WithParameter("queueName", QueueName);
+
+            builder.RegisterType<CustomerPhoneVerifiedEventSubscriber>()
+                .As<IStartStop>()
+                .WithParameter("connectionString", _settings.CustomerRabbitMqConnectionString)
+                .WithParameter("exchangeName", CustomerPhoneVerifiedEventExchangeName)
+                .WithParameter("queueName", QueueName);
+        }
+    }
+}
---
+++ src/MAVN.Service.DashboardStatistics/Modules/ServiceModule.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using JetBrains.Annotations;
+using Lykke.Sdk;
+using Lykke.SettingsReader;
+using MAVN.Job.TokensStatistics.Client;
+using MAVN.Service.DashboardStatistics.Services;
+using MAVN.Service.DashboardStatistics.Settings;
+
+namespace MAVN.Service.DashboardStatistics.Modules
+{
+    [UsedImplicitly]
+    public class ServiceModule : Module
+    {
+        private readonly AppSettings _settings;
+
+        public ServiceModule(IReloadingManager<AppSettings> settings)
+        {
+            _settings = settings.CurrentValue;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StartupManager>()
+                .As<IStartupManager>()
+                .SingleInstance();
+
+            builder.RegisterType<ShutdownManager>()
+                .As<IShutdownManager>()
+                .AutoActivate()
+                .SingleInstance();
+
+            builder.RegisterModule(new DomainServices.AutofacModule());
+            builder.RegisterModule(new MsSqlRepositories.AutofacModule(_settings.DashboardStatisticsService.Db.MsSqlConnectionString));
+
+            // Clients
+
+            builder.RegisterTokensStatisticsClient(_settings.TokensStatisticsJobClient, null);
+        }
+    }
+}
