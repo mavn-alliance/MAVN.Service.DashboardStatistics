eb7894ffa4ee15e9c39829ace8ccad8dc4589668
anna lizunova <a.lizunova@itransition.com>
2020-06-04 16:28:59 +0300

    Add missed XML comments

--- client/MAVN.Service.DashboardStatistics.Client/Models/VoucherStatistic/VoucherStatisticsResponse.cs
+++ client/MAVN.Service.DashboardStatistics.Client/Models/VoucherStatistic/VoucherStatisticsResponse.cs
@@ -1,15 +1,33 @@
 ﻿namespace MAVN.Service.DashboardStatistics.Client.Models.VoucherStatistic
 {
+    /// <summary>
+    /// Voucher statistics response
+    /// </summary>
     public class VoucherStatisticsResponse
     {
+        /// <summary>
+        /// Currency
+        /// </summary>
         public string Currency { get; set; }
 
+        /// <summary>
+        /// Total purchases cost
+        /// </summary>
         public decimal TotalPurchasesCost { get; set; }
 
+        /// <summary>
+        /// Total purchases count
+        /// </summary>
         public int TotalPurchasesCount { get; set; }
 
+        /// <summary>
+        /// Total redeemed vouchers cost
+        /// </summary>
         public decimal TotalRedeemedVouchersCost { get; set; }
 
+        /// <summary>
+        /// Total redeemed vouchers count
+        /// </summary>
         public int TotalRedeemedVouchersCount { get; set; }
     }
 }
